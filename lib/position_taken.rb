12ee832ebeb45584f66d237cfd81a7e1a26b1a65
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-23 03:10:34 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,11 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  isHere == false
   if board[index] == " " || board[index] == ""
-    ishere
+    false
   elsif board[index] == nif
-    isHere
+    false
   else
-    !isHere
+    true
   end
 end
